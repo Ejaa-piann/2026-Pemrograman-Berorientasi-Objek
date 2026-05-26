0ff0a3c3e7f34c6f733a65339d551e4a6f1c0e20
Alvian Reza <rezsyt2610@gmail.com>
2026-05-26 23:22:06 +0700

    Delete uas 3.py

--- uas 3.py
+++
@@ -1,8 +0,0 @@
-angka = [10, 20, 30, 40]
-print("List awal:", angka)
-
-# Menghapus elemen terakhir
-angka_terambil = angka.pop()
-
-print("Angka yang diambil:", angka_terambil)
-print("List setelah pop:", angka)
