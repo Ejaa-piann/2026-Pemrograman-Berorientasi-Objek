d1dba353f3a1e6765c6df4aee22e350e3bb678f0
Alvian Reza <rezsyt2610@gmail.com>
2026-05-26 23:15:52 +0700

    Delete Tebak angka berapa hayoo.py

--- Tebak angka berapa hayoo.py
+++
@@ -1,8 +0,0 @@
-#Tebak Angka
-print("--Mari Kita Tebak Angka--")
-tebak= int(input("Masukkan angka tebakan (1-100)= "))
-
-if tebak != 5:
-    print("Anjay Kelas Kids Benar")
-else : 
-    print("Dih Cupu Gitu aja salah")
