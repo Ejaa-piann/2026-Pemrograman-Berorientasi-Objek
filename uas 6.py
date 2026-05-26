82e57ff8591f92cf3aae590a5b26da441c0b1562
Alvian Reza <rezsyt2610@gmail.com>
2026-05-26 23:19:14 +0700

    Delete uas 6.py

--- uas 6.py
+++
@@ -1,8 +0,0 @@
-# Menulis teks ke file dengan mode 'w'
-with open('log_file.txt', 'w') as file:
-    file.write("Data Praktikum Selesai")
-
-# Membaca dan mencetak isi file
-with open('log_file.txt', 'r') as file:
-    isi = file.read()
-    print(isi)
